6be4a1dbe89bc208edd3534c21ff4d35edc152fa
kelemenkf <kelemenkf@gmail.com>
2022-04-24 13:29:42 +0200

    first 3 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    
+    let i = 0;
+    let result = ''
 
+    if(num < 0) {
+        return "ERROR";
+    } else {
+        while(i < num) {
+            result += string;
+            i++;
+    }
+
+    return result;
+    }
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split("");
+    let revereArray = stringArray.reverse();
+    let result = revereArray.join("")
+    return result;
 };
 
 // Do not edit below this line
