b1d9f8001b525525b95e6394c45fa7b571de5a17
kelemenkf <kelemenkf@gmail.com>
2022-04-24 18:00:00 +0200

    almost done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...items) {
+    for (let i = 0; i < array.length; i++) {
 
+        if (items.includes(array[i])) {
+            array.splice(i, 1);
+            i--;
+        } else if (!(array[i] in items)) {
+            continue;
+        } 
+    } 
+    return array;
 };
-
+// input an array, remove the item given to be removed. 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+
+    let sum = 0;
+
+    let parType1 = typeof a;
+    let parType2 = typeof b;
+
+
+    if (parType1 !==  'number' || parType2 !== 'number' || a < 0 || b < 0) {
+        return "ERROR";
+    } else if (a < b) {
+        for (let i = a; i <= b; i++) {
+            sum += i;
+        }
+        return sum;
+    } else if (a > b) {
+        for (let i = a; i >= b; i--) {
+            sum += i;
+        }
+        return sum;
+    }
 
 };
 
